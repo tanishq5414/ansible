e25a2c2fb035fdc80daf22cf43c0c8862ef135e9
Tanishq Agarwal <tanishqagarwal5414@gmail.com>
2024-07-29 20:26:49 +0530

    added ansible

--- README.md
+++
@@ -1,3 +0,0 @@
-# ansible
-My ansible playbooks
-
--- roles/neovim/files/lua/core/keymaps.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-vim.keymap.set("n", "<C-BS>", "dw", {silent = true, desc = 'delete from cursor to ending word'})
-vim.keymap.set("i", "<C-BS>", "dw", {silent = true, desc = 'delete from cursor to ending word'})
